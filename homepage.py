d58e21c57a0b46191532c108987da703eafb4a3d
Richard Xie <richardxy@gmail.com>
2020-04-24 20:24:21 -0400

    optimize data loading

--- homepage.py
+++ homepage.py
@@ -6,7 +6,7 @@ from dash.dependencies import Output, Input
 from scipy.signal import savgol_filter
 import utilities as utl
 from init import app
-
+import numpy as np 
 from navbar import Navbar
 
 nav = Navbar()
@@ -21,34 +21,40 @@ def load_case_list(region="US"):
     data_list_confirmed, data_list_deaths, data_list_recovered, date_list, region_of_interest = utl.load_data_2()
     return data_list_confirmed[region]
 
+def load_case_list_regions(): ## return data for all retions-of-interest
+    data_list_confirmed, data_list_deaths, data_list_recovered, date_list, region_of_interest = utl.load_data_2()
+    return data_list_confirmed, data_list_deaths, data_list_recovered, date_list, region_of_interest 
+
 
 def load_death_list(region="US"):
     data_list_confirmed, data_list_deaths, data_list_recovered, date_list, region_of_interest = utl.load_data_2()
     return data_list_deaths[region]
 
 
-def compute_increase_rate(region='US'):
-    print("...computing increase rate...\n")
-    data_list_confirmed, date_list = utl.load_data_3(region)
-    A = data_list_confirmed
+def compute_increase_rate(data_list_confirmed, region='US'):
+    # print("...computing increase rate...\n")
+    # data_list_confirmed, date_list = utl.load_data_3(region)
+    A = data_list_confirmed[region]
     # print("confirmed data is ", A)
-    rate = [(A[k + 1] - A[k]) / A[k] * 100 for k in range(0, len(A) - 1)]
+    # rate = [(A[k + 1] - A[k]) / A[k] * 100 for k in range(0, len(A) - 1)]
+    rate = np.diff(np.array(A))
     return rate
 
 
-def compute_death_increase_rate(region='US'):
+def compute_death_increase_rate(data_list_deaths, region='US'):
     print("...computing death increase rate...\n")
-    data_list_deaths, date_list = utl.load_data_4(region)
-    A = data_list_deaths
+    # data_list_deaths, date_list = utl.load_data_4(region)
+    A = data_list_deaths[region]
     # print("confirmed data is ", A)
-    rate = [(A[k + 1] - A[k]) / A[k] * 100 if A[k] > 0 else 0 for k in range(0, len(A) - 1)]
+    # rate = [(A[k + 1] - A[k]) / A[k] * 100 if A[k] > 0 else 0 for k in range(0, len(A) - 1)]
+    rate = np.diff(np.array(A))
     return rate
 
 
 def load_date_list_2(region='US'):
     data_list_confirmed, date_list = utl.load_data_3(region)
-    print("region is {}, date from {} ~ {}".format(
-        region, date_list[0], date_list[-1]))
+    # print("region is {}, date from {} ~ {}".format(
+    #     region, date_list[0], date_list[-1]))
 
     return date_list[1:]
 
@@ -58,24 +64,25 @@ def smooth_list(l, window=3, poly=1):
 
 
 def update_increase_rate_row(region="US"):
+    data_list_confirmed, data_list_deaths, data_list_recovered, date_list, region_of_interest = load_case_list_regions()
     return dbc.Row([dbc.Col([
         dcc.Graph(id='increase rate',
-                  figure={"data": [{"x": load_date_list_2(region),
-                                    "y": compute_increase_rate(region),
+                  figure={"data": [{"x": date_list[1:], # load_date_list_2(region),
+                                    "y": compute_increase_rate(data_list_confirmed, region),
                                     'mode': "lines+markers",
                                     'name': region + " Confirmed"},
-                                   {"x": load_date_list_2(region),
-                                    "y": smooth_list(compute_increase_rate(region),
+                                   {"x": date_list[1:], #load_date_list_2(region),
+                                    "y": smooth_list(compute_increase_rate(data_list_confirmed, region),
                                                      9,
                                                      2),
                                     'mode': 'lines+markers',
                                     'name': 'Smoothed Confirmed'},
-                                   {"x": load_date_list_2(region),
-                                    "y": compute_death_increase_rate(region),
+                                   {"x": date_list[1:], #load_date_list_2(region),
+                                    "y": compute_death_increase_rate(data_list_deaths,region),
                                     'mode': "lines+markers",
                                     'name': region + " Death"},
-                                   {"x": load_date_list_2(region),
-                                    "y": smooth_list(compute_death_increase_rate(region),
+                                   {"x": date_list[1:], # load_date_list_2(region),
+                                    "y": smooth_list(compute_death_increase_rate(data_list_deaths,region),
                                                      9,
                                                      2),
                                     'mode': "lines+markers",
@@ -92,15 +99,15 @@ def update_increase_rate_row(region="US"):
     ])
 
 
-def confirmed_vs_death(region="US"):
+def confirmed_vs_death(data_list_confirmed, data_list_deaths, data_list_recovered, date_list, region_of_interest,region="US"):
     return dbc.Row([dbc.Col([
         dcc.Graph(id='confirmed cases vs death',
-                  figure={"data": [{"x": load_date_list(),
-                                    "y": load_case_list("US"),
+                  figure={"data": [{"x": date_list, #load_date_list(),
+                                    "y": data_list_confirmed['US'], # load_case_list("US"),
                                     'mode': "lines+markers",
                                     'name': 'Confirmed Cases'},
-                                   {"x": load_date_list(),
-                                    "y": load_death_list("US"),
+                                   {"x": date_list, #load_date_list(),
+                                    "y": data_list_deaths, #load_death_list("US"),
                                     'mode': "lines+markers",
                                     'name': 'Death Cases'},
                                    ],
@@ -115,31 +122,28 @@ def confirmed_vs_death(region="US"):
     ])
 
 
-def load_table():
+def load_table(data_list_confirmed, data_list_deaths, data_list_recovered, date_list, region_of_interest):
     return html.Div(
         [
             dbc.Row([
                 dbc.Col([
-                    html.Div(html.H1("CADSEA - Covid 19 Analysis")
-                             ),
-                ]
-                )
+                    html.Div(html.H1("CADSEA - Covid 19 Analysis")),
+                ])
             ]
             ),
-            dbc.Row([
-                dbc.Col([
-                    html.Div(html.P("Dashboard in Python")
-                             ),
-                ]
-                )
-            ]
-            ),
+            # dbc.Row([
+            #     dbc.Col([
+            #         html.Div(html.P("Dashboard in Python")),
+            #     ]
+            #     )
+            # ]
+            # ),
             dbc.Row([
                 dbc.Col([
                     html.Span(
                         [
-                            html.H5(["US Death", dbc.Badge(load_death_list("US")[-1], color="danger", className="ml-1"),
-                                     dbc.Badge("+" + str(load_death_list("US")[-1] - load_death_list("US")[-2]),
+                            html.H5(["US Death", dbc.Badge(data_list_deaths["US"][-1], color="danger", className="ml-1"),
+                                     dbc.Badge("+" + str(data_list_deaths["US"][-1] - data_list_deaths["US"][-2]),
                                                color="danger",
                                                className="ml-1")]),
                         ]
@@ -150,8 +154,8 @@ def load_table():
                     html.Span(
                         [
                             html.H5(
-                                ["US Confirmed", dbc.Badge(load_case_list("US")[-1], color="warning", className="ml-1"),
-                                 dbc.Badge("+" + str(load_case_list("US")[-1] - load_case_list("US")[-2]),
+                                ["US Confirmed", dbc.Badge(data_list_confirmed["US"][-1], color="warning", className="ml-1"),
+                                 dbc.Badge("+" + str(data_list_confirmed["US"][-1] - data_list_confirmed["US"][-2]),
                                            color="warning",
                                            className="ml-1")]),
                         ]
@@ -165,8 +169,8 @@ def load_table():
                     html.Span(
                         [
                             html.H5(["Italy Death",
-                                     dbc.Badge(load_death_list("Italy")[-1], color="danger", className="ml-1"),
-                                     dbc.Badge("+" + str(load_death_list("Italy")[-1] - load_death_list("Italy")[-2]),
+                                     dbc.Badge(data_list_deaths["Italy"][-1], color="danger", className="ml-1"),
+                                     dbc.Badge("+" + str(data_list_deaths["Italy"][-1] - data_list_deaths["Italy"][-2]),
                                                color="danger",
                                                className="ml-1")]),
                         ]
@@ -178,8 +182,8 @@ def load_table():
                         [
                             html.H5(
                                 ["Italy Confirmed",
-                                 dbc.Badge(load_case_list("Italy")[-1], color="warning", className="ml-1"),
-                                 dbc.Badge("+" + str(load_case_list("Italy")[-1] - load_case_list("Italy")[-2]),
+                                 dbc.Badge(data_list_confirmed["Italy"][-1], color="warning", className="ml-1"),
+                                 dbc.Badge("+" + str(data_list_confirmed["Italy"][-1] - data_list_confirmed["Italy"][-2]),
                                            color="warning",
                                            className="ml-1")]),
                         ]
@@ -193,8 +197,8 @@ def load_table():
                     html.Span(
                         [
                             html.H5(["China Death",
-                                     dbc.Badge(load_death_list("China")[-1], color="danger", className="ml-1"),
-                                     dbc.Badge("+" + str(load_death_list("China")[-1] - load_death_list("China")[-2]),
+                                     dbc.Badge(data_list_deaths["China"][-1], color="danger", className="ml-1"),
+                                     dbc.Badge("+" + str(data_list_deaths["China"][-1] - data_list_deaths["China"][-2]),
                                                color="danger",
                                                className="ml-1")]),
                         ]
@@ -206,8 +210,8 @@ def load_table():
                         [
                             html.H5(
                                 ["China Confirmed",
-                                 dbc.Badge(load_case_list("China")[-1], color="warning", className="ml-1"),
-                                 dbc.Badge("+" + str(load_case_list("China")[-1] - load_case_list("China")[-2]),
+                                 dbc.Badge(data_list_confirmed["China"][-1], color="warning", className="ml-1"),
+                                 dbc.Badge("+" + str(data_list_confirmed["China"][-1] - data_list_confirmed["China"][-2]),
                                            color="warning",
                                            className="ml-1")]),
                         ]
@@ -221,8 +225,8 @@ def load_table():
                     html.Span(
                         [
                             html.H5(["Canada Death",
-                                     dbc.Badge(load_death_list("Canada")[-1], color="danger", className="ml-1"),
-                                     dbc.Badge("+" + str(load_death_list("Canada")[-1] - load_death_list("Canada")[-2]),
+                                     dbc.Badge(data_list_deaths["Canada"][-1], color="danger", className="ml-1"),
+                                     dbc.Badge("+" + str(data_list_deaths["Canada"][-1] - data_list_deaths["Canada"][-2]),
                                                color="danger",
                                                className="ml-1")]),
                         ]
@@ -234,8 +238,8 @@ def load_table():
                         [
                             html.H5(
                                 ["Canada Confirmed",
-                                 dbc.Badge(load_case_list("Canada")[-1], color="warning", className="ml-1"),
-                                 dbc.Badge("+" + str(load_case_list("Canada")[-1] - load_case_list("Canada")[-2]),
+                                 dbc.Badge(data_list_confirmed["Canada"][-1], color="warning", className="ml-1"),
+                                 dbc.Badge("+" + str(data_list_confirmed["Canada"][-1] - data_list_confirmed["Canada"][-2]),
                                            color="warning",
                                            className="ml-1")]),
                         ]
@@ -286,12 +290,13 @@ def load_table():
 
 
 # define the body layout
-def load_body():
+def load_body(data_list_confirmed, data_list_deaths, data_list_recovered, date_list, region_of_interest):
+    
     return html.Div(
         [
             dbc.Row([
                 dbc.Col([
-                    html.Div(confirmed_vs_death('US'),
+                    html.Div(confirmed_vs_death(data_list_confirmed, data_list_deaths, data_list_recovered, date_list, region_of_interest),
                              ),
                 ]
                 ),
@@ -313,13 +318,14 @@ def load_body():
     Output('confirmed cases vs death', 'figure'),
     [Input('Region_of_interest', 'value')])
 def update_figure(value):
+    data_list_confirmed, data_list_deaths, data_list_recovered, date_list, region_of_interest = load_case_list_regions()
     return {
-        "data": [{"x": load_date_list(),
-                  "y": load_case_list(value),
+        "data": [{"x": date_list,
+                  "y": data_list_confirmed[value],
                   'mode': "lines+markers",
                   'name': 'Confirmed Cases'},
-                 {"x": load_date_list(),
-                  "y": load_death_list(value),
+                 {"x": date_list,
+                  "y": data_list_deaths[value],
                   'mode': "lines+markers",
                   'name': 'Death Cases'},
                  ],
@@ -333,23 +339,24 @@ def update_figure(value):
     Output('increase rate', 'figure'),
     [Input('Region_of_interest', 'value')])
 def update_figure(value):
+    data_list_confirmed, data_list_deaths, data_list_recovered, date_list, region_of_interest = load_case_list_regions()
     return {
-        "data": [{"x": load_date_list_2(value),
-                  "y": compute_increase_rate(value),
+        "data": [{"x": date_list,
+                  "y": compute_increase_rate(data_list_confirmed,value),
                   'mode': "lines+markers",
                   'name': value + " Confirmed"},
-                 {"x": load_date_list_2(value),
-                  "y": smooth_list(compute_increase_rate(value),
+                 {"x": date_list,
+                  "y": smooth_list(compute_increase_rate(data_list_confirmed, value),
                                    9,
                                    2),
                   'mode': 'lines+markers',
                   'name': 'Smoothed Confirmed'},
-                 {"x": load_date_list_2(value),
-                  "y": compute_death_increase_rate(value),
+                 {"x": date_list,
+                  "y": compute_death_increase_rate(data_list_deaths,value),
                   'mode': "lines+markers",
                   'name': value + " Death"},
-                 {"x": load_date_list_2(value),
-                  "y": smooth_list(compute_death_increase_rate(value),
+                 {"x": date_list,
+                  "y": smooth_list(compute_death_increase_rate(data_list_deaths,value),
                                    9,
                                    2),
                   'mode': "lines+markers",
@@ -362,10 +369,11 @@ def update_figure(value):
 
 
 def load_layout():
+    data_list_confirmed, data_list_deaths, data_list_recovered, date_list, region_of_interest = load_case_list_regions()
     layout = html.Div([
         nav,
-        load_table(),
-        load_body(),
+        load_table(data_list_confirmed, data_list_deaths, data_list_recovered, date_list, region_of_interest),
+        load_body(data_list_confirmed, data_list_deaths, data_list_recovered, date_list, region_of_interest),
     ])
     return layout
 
